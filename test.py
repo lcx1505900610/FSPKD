be23e1e3a78807e47acd5050f67a448823881894
lichangxing <84111888+lcx1505900610@users.noreply.github.com>
2023-11-24 14:08:08 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # -*- codeing = utf-8 -*-
 # @Time : 2023/10/26 22:58
 # @Author : 李昌杏
-# @File : extract_feature.py
+# @File : test.py
 # @Software : PyCharm
 import argparse
 import multiprocessing
@@ -11,15 +11,11 @@ from joblib import Parallel, delayed
 from torch import nn
 from torch.utils.data import DataLoader
 import os
-
 from tqdm import tqdm
-
 import network.student as vits
 from datasets import load_data
 import numpy as np
 import torch.nn.functional as F
-
-from network.teacher import ModalityFusionNetwork
 from utils.utils import map_sake, prec_sake
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
@@ -110,31 +106,6 @@ def mAP1(photo_loader, sketch_loader, model_img,model_skt,k):
                    test_features_skt.numpy(), gallery_labels_skt.numpy(),sim,k=k['precision'])
     print('map{}: {:.4f} prec{}: {:.4f}'.format(k['map'], np.mean(aps), k['precision'], prec))
 
-def evaluate_teacher_class(args):
-    datasets, sk_valid_data, im_valid_data = load_data(args)
-    num_class = datasets.get_num_class()
-    model= ModalityFusionNetwork(args.img_size, 3, feature_dim=args.teacher_out_dim, heads=3,encoder_backbone=args.teacher_encoder,
-                                    num_class=num_class,
-                                    checkpoint_path=args.teacher_pre_weight).cuda()
-    model.load_state_dict(torch.load(f'../weights/teacher_{args.dataset}_good.pth'))
-    model.eval()
-    data_loader=DataLoader(datasets, batch_size=32, shuffle=True, num_workers=2, pin_memory=True)
-    acc_img=0
-    acc_skt=0
-
-    for data in data_loader:
-        sk, im, sketch_neg, image_neg,label,label_neg= data
-        sk, im,label = sk.cuda(), im.cuda(),label.cuda()
-
-        photo1_cls,sketch1_cls,photo1_f,sketch1_f = model(im, sk)
-
-        photo_out=torch.argmax(F.softmax(photo1_f,dim=1),dim=1)
-        sketch_out=torch.argmax(F.softmax(sketch1_f,dim=1),dim=1)
-
-        acc_skt+=sum(sketch_out==label)
-        acc_img+=sum(photo_out==label)
-    print(f'img class acc{acc_img/len(datasets)}\nskt class acc{acc_skt/len(datasets)}')
-
 def evaluate_student(args):
     datasets, sk_valid_data, im_valid_data = load_data(args)
     model_skt = vits.Student_SKT(datasets.get_num_class(),checkpoint_path='../weights/vit.npz').cuda()
@@ -155,53 +126,6 @@ def valid(skt_loader, img_loader,model_img,model_skt):
     model_img.train()
     model_skt.train()
     return acc
-
-def mAP_visual(photo_loader, sketch_loader, model_img,model_skt ,k=10):
-    import cv2
-    gallery_reprs = []
-    gallery_labels = []
-    gallery_photo = []
-    with torch.no_grad():
-        for photo, label,org in photo_loader:
-            photo, label = photo.cuda(), label.cuda()
-            photo_reprs = model_img.embedding(photo)
-            gallery_reprs.append(photo_reprs)
-            gallery_labels.append(label)
-            gallery_photo.append(org)
-        gallery_reprs = F.normalize(torch.cat(gallery_reprs))
-        gallery_labels = torch.cat(gallery_labels)
-        gallery_photo=torch.cat(gallery_photo)
-
-        for sketch, label,org in sketch_loader:
-            sketch, label = sketch.cuda(), label.cuda()
-            sketch_reprs = F.normalize(model_skt.embedding(sketch))
-            ranks = torch.argsort(torch.matmul(sketch_reprs, gallery_reprs.T), dim=1, descending=True).cpu()
-            retrievals_photo = gallery_photo[ranks[:, :k]]
-            for sketch_idx, retrieval_photo in enumerate(retrievals_photo):
-                s=org[sketch_idx]
-                print(s.shape)
-                p=retrieval_photo
-                pic=[]
-                pic.append(s)
-                for _ in p:pic.append(_)
-                img=np.concatenate(pic,axis=1)
-                print(img.shape)
-                cv2.imwrite(f'{sketch_idx}.png',np.array(img))
-
-            return 0
-        return 0
-
-def visual_student(args):
-    datasets, sk_valid_data, im_valid_data = load_data(args,org=True)
-    model_skt = vits.Student_SKT(datasets.get_num_class(),checkpoint_path='../weights/vit.npz').cuda()
-    model_img = vits.Student_IMG(datasets.get_num_class(),checkpoint_path='../weights/vit.npz').cuda()
-    model_skt.load_state_dict(torch.load('weights/student_skt_good.pth'))
-    model_img.load_state_dict(torch.load('weights/student_img_good.pth'))
-    model_skt.eval()
-    model_img.eval()
-    skt_loader = DataLoader(sk_valid_data, batch_size=6, shuffle=True, num_workers=2, pin_memory=True)
-    img_loader = DataLoader(im_valid_data, batch_size=256, shuffle=True, num_workers=2, pin_memory=True)
-    print(mAP_visual(img_loader,skt_loader ,model_img,model_skt,k=10))
 
 class Option:
     def __init__(self):
@@ -239,6 +163,4 @@ class Option:
 
 if __name__ == '__main__':
     args=Option().parse()
-    # evaluate_teacher_class(args)
     evaluate_student(args)
-    # visual_student(args)
